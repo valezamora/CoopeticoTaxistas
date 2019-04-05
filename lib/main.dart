c6cd59d909ef59a93ec32bd0c2e77e92e1700412
Marco <marco.venegas98@hotmail.com>
2019-04-05 13:28:13 -0600

    Creo ventana login de usuario
    Elimino la de taxista que estaba en este repo. Luego se guardará en el repo de la aplicación de taxistas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import './screens/login_taxista.dart';
+import './screens/login_usuario.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
         fontFamily: "Roboto"
       ),
-      home: LoginTaxista(titulo: "Login Taxista.")//RandomWords(),
+      home: LoginUsuario(titulo: "Login Usuario.")//RandomWords(),
     );
   }
 }
