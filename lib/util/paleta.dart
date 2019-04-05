f1a4fa1c281f7f018947de91a5cf42bf108ca0bd
Marco <marco.venegas98@hotmail.com>
2019-04-04 21:56:35 -0600

    Paleta de colores

---
+++ lib/util/paleta.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class Paleta {
+  static Color Naranja = Color(0xFFF7A94B);
+  static Color Verde = Color(0xFF6FCF97);
+  static Color Rojo = Color(0xFFFE6969);
+  static Color Gris = Color(0xFF828282);
+}
