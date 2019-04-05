801b4a3c4e521e5133a3f0f17b29f345e1164458
Marco <marco.venegas98@hotmail.com>
2019-04-04 23:33:58 -0600

    Librerías para importar más fácil

---
+++ lib/util/util_lib.dart
@@ -0,0 +1,7 @@
+//Librería hecha a mano para no tener que importar cada util por separado.
+//Agregar una entrada export para cada util.
+
+library util_lib;
+
+export "paleta.dart";
+export "tamano_letra.dart";
