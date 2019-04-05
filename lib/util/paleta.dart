7e12f4114613265975549178d3f51d691dd61602
Marco <marco.venegas98@hotmail.com>
2019-04-05 08:36:30 -0600

    Agrego color azul para los "hyperlinks"

--- lib/util/paleta.dart
+++ lib/util/paleta.dart
@@ -5,4 +5,5 @@ class Paleta {
   static Color Verde = Color(0xFF6FCF97);
   static Color Rojo = Color(0xFFFE6969);
   static Color Gris = Color(0xFF828282);
+  static Color Azul = Color(0xFF0000EE);
 }
