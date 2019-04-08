9199ae2a8394fa63591d727a5f10e1c4d047bdb1
Marco <marco.venegas98@hotmail.com>
2019-04-08 14:30:11 -0600

    Deshabilito la ruta / pues se especifica el home en el main

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,11 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:CoopeticoApp/screens/login_usuario.dart';
+import 'package:CoopeticoApp/screens/temp.dart';
 
 //import 'package:CoopeticoApp/screens/home.dart';
 
 final routes = {
   '/login':         (BuildContext context) => new LoginUsuario(),
-  //'/home':         (BuildContext context) => new HomeScreen(),
-  '/' :          (BuildContext context) => new LoginUsuario(),
+  '/home':         (BuildContext context) => new Temp(),
+  //'/' :          (BuildContext context) => new LoginUsuario(),
 };
