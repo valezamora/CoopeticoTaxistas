26e6748fb13f6dbdfa773eb6948db440e5d54e8f
Marco <marco.venegas98@hotmail.com>
2019-04-09 10:17:27 -0600

    Elimino util_lib y widgets_lib porque aparentemente es inseguro.

--- lib/util/util_lib.dart
+++
@@ -1,7 +0,0 @@
-//Librería hecha a mano para no tener que importar cada util por separado.
-//Agregar una entrada export para cada util.
-
-library util_lib;
-
-export "paleta.dart";
-export "tamano_letra.dart";
