6d03ab1bc8f133c0c6b68f01f2822a0547ccddb9
Valeria <valezamora@gmail.com>
2019-04-22 11:06:54 -0600

    Componente para texto del perfil del taxista.

--- lib/widgets/info_usuario.dart
+++ lib/widgets/info_usuario.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:CoopeticoTaxiApp/util/paleta.dart';
-import 'package:CoopeticoTaxiApp/util/tamano_letra.dart';
+import 'package:CoopeticoTaxiApp/widgets/texto_perfil.dart';
 
 /// Clase que muestra los datos personales del usuario.
 ///
@@ -19,56 +19,17 @@ class InfoUsuario extends StatelessWidget {
           color: Paleta.Gris,
           height: 40,
         ),
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-            padding: new EdgeInsets.only(left: 20.0),
-            child: Text('Nombre', textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.bold, color: Paleta.Gris, fontSize: TamanoLetra.H2)),
-          )
-        ),
-        SizedBox(
-            width: double.infinity,
-            child: Container(
-              padding: new EdgeInsets.only(left: 35.0, top: 10.0),
-              child: Text(this.nombre, textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.normal, color: Paleta.Gris, fontSize: TamanoLetra.H2)),
-            )
-        ),
+        TextoPerfil("Nombre", nombre),
         Divider(
           color: Paleta.Gris,
           height: 40,
         ),
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-            padding: new EdgeInsets.only(left: 20.0),
-            child: Text('Teléfono', textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.bold, color: Paleta.Gris, fontSize: TamanoLetra.H2)),
-          )
-        ),
-        SizedBox(
-            width: double.infinity,
-            child: Container(
-              padding: new EdgeInsets.only(left: 35.0, top: 10.0),
-              child: Text(this.telefono, textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.normal, color: Paleta.Gris, fontSize: TamanoLetra.H2)),
-            )
-        ),
+        TextoPerfil("Teléfono", telefono),
         Divider(
           color: Paleta.Gris,
           height: 40,
         ),
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-            padding: new EdgeInsets.only(left: 20.0),
-            child: Text('Correo', textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.bold, color: Paleta.Gris, fontSize: TamanoLetra.H2)),
-          )
-        ),
-        SizedBox(
-            width: double.infinity,
-            child: Container(
-              padding: new EdgeInsets.only(left: 35.0, top: 10.0),
-              child: Text(this.correo, textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.normal, color: Paleta.Gris, fontSize: TamanoLetra.H2)),
-            )
-        ),
+        TextoPerfil("Correo", correo),
         Divider(
           color: Paleta.Gris,
           height: 40,
---
+++ lib/widgets/texto_perfil.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:CoopeticoTaxiApp/util/tamano_letra.dart';
+import 'package:CoopeticoTaxiApp/util/paleta.dart';
+
+/// Clase que muestra una categoría de datos.
+/// [id] el nombre del campo que se muestra
+/// [texto] el contenido asociado al id
+///
+/// Autor: Valeria Zamora
+class TextoPerfil extends StatelessWidget {
+  final String texto;
+  final String id;
+
+  TextoPerfil(this.id, this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+            width: double.infinity,
+            child: Container(
+              padding: new EdgeInsets.only(left: 20.0),
+              child: Text(id,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Paleta.Gris,
+                      fontSize: TamanoLetra.H2
+                  )
+              ),
+            )
+        ),
+        SizedBox(
+            width: double.infinity,
+            child: Container(
+              padding: new EdgeInsets.only(left: 35.0, top: 10.0),
+              child: Text(texto, textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.normal, color: Paleta.Gris, fontSize: TamanoLetra.H2)),
+            )
+        ),
+      ],
+    );
+  }
+}
