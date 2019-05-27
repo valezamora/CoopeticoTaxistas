692d82eda9b089313109ffe89d278d799d7ab9d2
valeriazamora <valezamora@gmail.com>
2019-05-27 15:50:42 -0600

    primer intento

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:CoopeticoTaxiApp/screens/direcci%C3%B3nOrigen.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 import 'package:CoopeticoTaxiApp/routes.dart';
 
 import 'package:CoopeticoTaxiApp/util/seleccionador_home.dart';
 
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
 void main() async{
+  // channel: IOWebSocketChannel.connect('ws://echo.websocket.org');
   runApp(CoopeticoAppTaxista(await SeleccionadorHome.seleccionarHome()));
 }
 
@@ -15,18 +20,21 @@ void main() async{
 ///
 class CoopeticoAppTaxista extends StatelessWidget {
   final Widget home;
+  // final WebSocketChannel channel;
 
   CoopeticoAppTaxista(this.home);
 
   @override
   Widget build(BuildContext context) {
+    var channel = IOWebSocketChannel.connect('ws://echo.websocket.org');
+    channel.sink.add('caca');
+    print('PROTOCOL' + channel.toString());
     return MaterialApp(
       title: 'Coopetico Taxi App',
       theme: new ThemeData(primaryColor: Colors.white, fontFamily: "Roboto"),
       home: home,
       routes: routes,
       debugShowCheckedModeBanner: false,
-
     );
   }
 }
